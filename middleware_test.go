089ce74d65f212ac975d601363ad5db6eecebcb1
David Cai <davidcai1993@yahoo.com>
2017-12-19 14:45:47 +0800

    test: optimize tests

--- middleware_test.go
+++ middleware_test.go
@@ -5,18 +5,19 @@ import (
 	"testing"
 
 	"github.com/DavidCai1993/request"
-
 	"github.com/stretchr/testify/suite"
 	"github.com/teambition/gear"
 )
 
 const (
-	Addr = ":28080"
+	Addr  = ":28080"
+	Addr2 = ":28081"
 )
 
 type MiddlewareSuite struct {
 	suite.Suite
-	app *gear.App
+	app             *gear.App
+	defualtQueryApp *gear.App
 }
 
 type S1 struct {
@@ -38,13 +39,61 @@ func (s *MiddlewareSuite) SetupSuite() {
 	go func() {
 		s.Require().Nil(s.app.Listen(Addr))
 	}()
+
+	s.defualtQueryApp = gear.New()
+	s.defualtQueryApp.Set(gear.SetSender, New(Options{}))
+	s.defualtQueryApp.Use(func(ctx *gear.Context) error {
+		a := "aaa"
+		b := 3
+		c := float64(2.2)
+
+		return ctx.Send(http.StatusOK, S1{A: &a, B: &b, C: &c})
+	})
+	go func() {
+		s.Require().Nil(s.defualtQueryApp.Listen(Addr2))
+	}()
 }
 
 func (s *MiddlewareSuite) TestVaildQuery() {
 	json, err := request.Get("http://127.0.0.1" + Addr + "?c=a,c").JSON(new(S1))
 
 	s.Nil(err)
+	s.Equal(*(json.(*S1).A), "aaa")
+	s.Nil(json.(*S1).B)
+	s.Equal(*(json.(*S1).C), float64(2.2))
+}
 
+func (s *MiddlewareSuite) TestOtherQuery() {
+	json, err := request.Get("http://127.0.0.1" + Addr + "?c=e,f").JSON(new(S1))
+
+	s.Nil(err)
+	s.Nil(json.(*S1).A)
+	s.Nil(json.(*S1).B)
+	s.Nil(json.(*S1).C)
+}
+
+func (s *MiddlewareSuite) TestEmptyQuery() {
+	json, err := request.Get("http://127.0.0.1" + Addr + "?c=").JSON(new(S1))
+
+	s.Nil(err)
+	s.Equal(*(json.(*S1).A), "aaa")
+	s.Equal(*(json.(*S1).B), 3)
+	s.Equal(*(json.(*S1).C), float64(2.2))
+}
+
+func (s *MiddlewareSuite) TestWithoutQuery() {
+	json, err := request.Get("http://127.0.0.1" + Addr + "?d=d,v&&e=f").JSON(new(S1))
+
+	s.Nil(err)
+	s.Equal(*(json.(*S1).A), "aaa")
+	s.Equal(*(json.(*S1).B), 3)
+	s.Equal(*(json.(*S1).C), float64(2.2))
+}
+
+func (s *MiddlewareSuite) TestDefaultQueryApp() {
+	json, err := request.Get("http://127.0.0.1" + Addr2 + "?fields=a,c").JSON(new(S1))
+
+	s.Nil(err)
 	s.Equal(*(json.(*S1).A), "aaa")
 	s.Nil(json.(*S1).B)
 	s.Equal(*(json.(*S1).C), float64(2.2))
